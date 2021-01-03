a55c1931f4a1fd6a65b0198bf1a8606985e4d156
mark <whitney.mark@gmail.com>
2021-01-04 09:28:04 +1000

    Support custom impersonation manager.

---
+++ src/Contracts/Impersonate.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace KABBOUCHI\NovaImpersonate\Contracts;
+
+interface Impersonate
+{
+    /**
+     * @return void
+     */
+    public function clear();
+
+    /**
+     * @param int $id
+     * @param string|null $guardName
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function findUserById($id, $guardName = null);
+
+    /**
+     * @return int|null
+     */
+    public function getImpersonatorId();
+
+    /**
+     * @return bool
+     */
+    public function isImpersonating();
+
+    /**
+     * @param \Illuminate\Contracts\Auth\Authenticatable $from
+     * @param \Illuminate\Contracts\Auth\Authenticatable $to
+     * @param string|null $guardName
+     * @return bool
+     */
+    public function take($from, $to, $guardName = null);
+
+    /**
+     * @return bool
+     */
+    public function leave();
+}
